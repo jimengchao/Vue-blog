6986bb37bff8eeabbbd4800009b6bbce8386389e
jimengchao <1097198549@qq.com>
2017-05-05 22:54:16 +0800

    vue-blog

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">
+      <!-- 菜单 -->
+      <Navbar></Navbar>
+  </div>
+</template>
+
+
+<script>
+import Navbar from "@/view/layout/Navbar"
+
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss">
+  .wrapper{
+      position: relative;
+      height: 100%;
+      width: 100%;
+      padding-left: 180px;
+  }
+  .wrapper:after{
+    content: "";
+    display: table;
+    clear: both;
+  }
+</style>
